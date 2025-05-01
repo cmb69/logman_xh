3e6442e7edc0ebe33b5dc7fb60d0035970abad4a
Christoph M. Becker <cmbecker69@gmx.de>
2025-05-01 23:39:41 +0200

    Simplify MainAdminTest setup

--- tests/MainAdminTest.php
+++ tests/MainAdminTest.php
@@ -5,96 +5,91 @@ namespace Logman;
 use ApprovalTests\Approvals;
 use Logman\Model\Entry;
 use Logman\Model\Logfile;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use Plib\FakeRequest;
 use Plib\View;
 
 class MainAdminTest extends TestCase
 {
+    /** @var array<string,string> */
+    private array $conf;
+
+    /** @var LogFile&MockObject */
+    private $logfile;
+
+    private View $view;
+
+    public function setUp(): void
+    {
+        $this->conf = XH_includeVar("./config/config.php", "plugin_cf")["logman"];
+        $this->logfile = $this->createStub(Logfile::class);
+        $this->view = new View("./views/", XH_includeVar("./languages/en.php", "plugin_tx")["logman"]);
+    }
+
+    private function sut()
+    {
+        return new MainAdmin($this->conf, $this->logfile, $this->view);
+    }
+
     public function testDisplaysLogfile(): void
     {
-        $logfile = $this->createMock(Logfile::class);
-        $logfile->expects($this->once())->method("find")->willReturn([
+        $this->logfile->expects($this->once())->method("find")->willReturn([
             $this->loginSuccessEntry(),
             $this->movedEntry(),
             $this->loginFailureEntry(),
         ]);
-        $sut = $this->sut($this->conf(), $logfile, $this->view());
         $request = new FakeRequest([
             "url" => "http://example.com/?&action=plugin_text&logman_timestamp=2025&logman_level=info&logman_module=XH"
                 . "&logman_category=login&logman_description=from",
         ]);
-        $response = $sut($request);
+        $response = $this->sut()($request);
         Approvals::verifyHtml($response->output());
     }
 
     /** <https://github.com/cmb69/logman_xh/issues/1> */
     public function testZeroMaxEntriesDisplaysEntries(): void
     {
-        $conf = $this->conf();
-        $conf["entries_max"] = "0";
-        $logfile = $this->createMock(Logfile::class);
-        $logfile->expects($this->once())->method("find")->with($this->anything(), PHP_INT_MAX)
+        $this->conf["entries_max"] = "0";
+        $this->logfile->expects($this->once())->method("find")->with($this->anything(), PHP_INT_MAX)
             ->willReturn([$this->loginSuccessEntry()]);
-        $sut = $this->sut($conf, $logfile, $this->view());
         $request = new FakeRequest([
             "url" => "http://example.com/?&action=plugin_text",
         ]);
-        $response = $sut($request);
+        $response = $this->sut()($request);
         $this->assertStringContainsString("2023-01-30 14:00:05", $response->output());
     }
 
     public function testDisplaysDeleteConfirmation(): void
     {
-        $logfile = $this->createMock(Logfile::class);
-        $sut = $this->sut($this->conf(), $logfile, $this->view());
         $request = new FakeRequest([
             "url" => "http://example.com/?&action=delete&logman_count=1",
         ]);
-        $response = $sut($request);
+        $response = $this->sut()($request);
         Approvals::verifyHtml($response->output());
     }
 
     public function testDeletesEntries(): void
     {
-        $logfile = $this->createMock(Logfile::class);
-        $logfile->expects($this->once())->method("delete")->willReturn(1);
-        $sut = $this->sut($this->conf(), $logfile, $this->view());
+        $this->logfile->expects($this->once())->method("delete")->willReturn(1);
         $request = new FakeRequest([
             "url" => "http://example.com/?&action=delete&logman_count=1",
             "post" => [
                 "logman_do" => "",
             ],
         ]);
-        $response = $sut($request);
+        $response = $this->sut()($request);
         $this->assertSame("http://example.com/?&logman_count=1&logman_deleted=1", $response->location());
     }
 
     public function testDisplaysNumberOfDeletedEntries(): void
     {
-        $logfile = $this->createMock(Logfile::class);
-        $logfile->expects($this->once())->method("find")->willReturn([$this->loginSuccessEntry()]);
-        $sut = $this->sut($this->conf(), $logfile, $this->view());
+        $this->logfile->expects($this->once())->method("find")->willReturn([$this->loginSuccessEntry()]);
         $request = new FakeRequest([
             "url" => "http://example.com/?&action=&logman_deleted=17",
         ]);
-        $response = $sut($request);
+        $response = $this->sut()($request);
         $this->assertStringContainsString("17 entries deleted!", $response->output());
-    }
-
-    private function sut(array $conf, Logfile $logfile, View $view)
-    {
-        return new MainAdmin($conf, $logfile, $view);
-    }
-
-    private function conf(): array
-    {
-        return XH_includeVar("./config/config.php", "plugin_cf")["logman"];
-    }
-
-    private function view(): View
-    {
-        return new View("./views/", XH_includeVar("./languages/en.php", "plugin_tx")["logman"]);
     }
 
     private function loginSuccessEntry(): Entry
